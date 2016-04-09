7129cd47002e29aa5bf785c6be02b99fa1034b5c
Christophe GRASSIOT <grassiot.christophe@gmail.com>
2016-04-09 14:59:20 +0200

    FIX Class MysqlDatabase
    Fix getPDO() for get info from /config/config.php

--- core/Database/MysqlDatabase.php
+++ core/Database/MysqlDatabase.php
@@ -20,7 +20,7 @@ class MysqlDatabase extends Database{
 
     private function getPDO(){
         if($this->pdo === null){
-            $pdo = new PDO('mysql:dbname=extranet_rubis;host=localhost', 'root', 'root');
+            $pdo = new PDO('mysql:dbname=' . $this->db_name . ';host=' . $this->db_host . '', $this->db_user,  $this->db_pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
